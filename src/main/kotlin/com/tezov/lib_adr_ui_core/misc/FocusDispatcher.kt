ce421856620e6bc1d4cdb9d13580f5534fe29914
tezov <tezov.app@gmail.com>
2023-11-01 19:13:35 +0100

    update focus dispatcher

--- src/main/kotlin/com/tezov/lib_adr_ui_core/misc/FocusDispatcher.kt
+++ src/main/kotlin/com/tezov/lib_adr_ui_core/misc/FocusDispatcher.kt
@@ -1,11 +1,12 @@
-
-
 package com.tezov.lib_adr_ui_core.misc
 
 import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.platform.SoftwareKeyboardController
@@ -13,39 +14,53 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
+@Composable
+fun rememberFocusDispatcher():FocusDispatcher {
+    return remember {
+        FocusDispatcher()
+    }.also { it.compose() }
+}
+
+fun Modifier.focusId(
+    id: FocusDispatcher.FocusId,
+) = focusRequester(id.value)
+    .onFocusChanged {
+        if (it.isFocused) id.onFocus()
+    }
+
 @OptIn(ExperimentalComposeUiApi::class)
 class FocusDispatcher {
 
-    inner class FocusId internal constructor(internal val autoShowKeyboard:Boolean){
+    inner class FocusId internal constructor(internal val autoShowKeyboard: Boolean) {
 
         val value: FocusRequester = FocusRequester()
 
-        fun onFocus() { onFocus(this) }
+        fun onFocus() {
+            onFocus(this)
+        }
 
-        fun hasFocus() = hasFocus(this)
+        val hasFocus get () = hasFocus(this)
 
         fun requestFocus() = requestFocus(this)
 
     }
 
-    private val ids = mutableListOf<com.tezov.lib_adr_ui_core.misc.FocusDispatcher.FocusId>()
+    private val ids = mutableListOf<FocusDispatcher.FocusId>()
     private var keyboardController: SoftwareKeyboardController? = null
     private lateinit var coroutine: CoroutineScope
     private lateinit var focusManager: FocusManager
-    private lateinit var focusOwner: MutableState<com.tezov.lib_adr_ui_core.misc.FocusDispatcher.FocusId?>
+    private lateinit var focusOwner: MutableState<FocusDispatcher.FocusId?>
 
-    fun createId(autoShowKeyboard:Boolean = true) = FocusId(autoShowKeyboard).also { ids.add(it) }
+    fun createId(autoShowKeyboard: Boolean = true) = FocusId(autoShowKeyboard).also { ids.add(it) }
 
-    fun destroyId(id: com.tezov.lib_adr_ui_core.misc.FocusDispatcher.FocusId) = ids.remove(id)
+    fun destroyId(id: FocusDispatcher.FocusId) = ids.remove(id)
 
     @Composable
     fun compose() {
         coroutine = rememberCoroutineScope()
         keyboardController = LocalSoftwareKeyboardController.current
         focusManager = LocalFocusManager.current
-        focusOwner = remember {
-            mutableStateOf(null)
-        }
+        focusOwner = remember { mutableStateOf(null) }
     }
 
     fun showKeyboard() {
@@ -65,26 +80,25 @@ class FocusDispatcher {
         hideKeyboard()
     }
 
-    private fun onFocus(id: com.tezov.lib_adr_ui_core.misc.FocusDispatcher.FocusId){
+    private fun onFocus(id: FocusDispatcher.FocusId) {
         focusOwner.value = id
-        if(id.autoShowKeyboard){
+        if (id.autoShowKeyboard) {
             showKeyboard()
-        }
-        else{
+        } else {
             hideKeyboard()
         }
     }
 
-    fun requestFocus(id: com.tezov.lib_adr_ui_core.misc.FocusDispatcher.FocusId){
+    fun requestFocus(id: FocusDispatcher.FocusId) {
         if (focusOwner.value != id) {
             kotlin.runCatching {
                 //TODO no idea why but throw "IllegalStateException" but the focus is obtained anyway ...
-                //maybe add focus cemetery same i did in java?
+                //maybe add focus cemetery same I did in java?
                 id.value.requestFocus()
             }
         }
     }
 
-    fun hasFocus(id: com.tezov.lib_adr_ui_core.misc.FocusDispatcher.FocusId) = focusOwner.value == id
+    fun hasFocus(id: FocusDispatcher.FocusId) = focusOwner.value == id
 
 }
